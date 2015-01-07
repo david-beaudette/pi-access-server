68fcf118d4dab401b47f199aa7b29711a4b3aa7f
david-beaudette <david.beaudette@usherbrooke.ca>
2015-01-07 16:55:17 -0500

    First test file

---
+++ tests/test_db_connect.py
@@ -0,0 +1,10 @@
+import string
+import MySQLdb
+
+db = MySQLdb.connect(host="box334.bluehost.com", # your host, usually localhost
+                     user="sherbro3_picrm", # your username
+                     passwd="Tp,5}g(~UQU=", # your password
+                     db="sherbro3_civicrm") # name of the data base
+
+cur = db.cursor()
+
