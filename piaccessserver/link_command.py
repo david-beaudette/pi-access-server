65f2d49b42ad43ae5ce82f58ce04905c0b5843b8
david-beaudette <david.beaudette@usherbrooke.ca>
2015-02-07 09:34:35 -0500

    Added required libraries to setup NRF24 on RPi.

---
+++ piaccessserver/link_command.py
@@ -0,0 +1,20 @@
+
+class link_command():
+    def __init__(self):
+
+        
+    def link_register():
+        pass
+
+    def link_enable():
+        pass
+
+    def link_disable():
+        pass
+
+    def link_dump_logging():
+        pass
+
+    def link_update_table():
+        pass
+
