b065009cbb8a710d4c27e851d20ddb5fa276776e
david-beaudette <david.beaudette@usherbrooke.ca>
2015-04-26 22:56:39 -0400

    Created main entry point for server methods.

--- piaccessserver/piaccessserver.py
+++ piaccessserver/piaccessserver.py
@@ -6,206 +6,132 @@
 
 import sys
 import ConfigParser
-import time
+from time import strftime
 import logging
 import argparse
-
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
-from lib_nrf24 import NRF24
-import time
-import spidev
+from glob import glob
+import shutil
+import os
 
 import db_connect
 import link_command
-import csv_csvw
+import csv_rw
 
-class PiAccessServer():
-    """ Main application class for the access server of the RFID card
-    control system.
-    """
-    def __init__(self, config_filename):
-        # Get server parameters
-        self.config_filename = config_filename
-        self.config = ConfigParser.RawConfigParser()    
-        self.config.read(self.config_filename)
-        
-        self.name = self.config.get('ACCESS_SERVER',
-                                    'name')
-        self.csv_filename = self.config.get('ACCESS_SERVER',
-                                            'csv_filename')
-        self.log_filename = self.config.get('ACCESS_SERVER',
-                                               'log_filename')
-        self.number_machines = self.config.get('MACHINES',
-                                               'number_machines')
-        self.mem_usage_threshold = self.config.getfloat('MACHINES',
-                                               'mem_usage_threshold')
-
-        # Initialise log file (time not added by default because machine
-        # log entries have a differed timestamp
-        logging.basicConfig(filename=self.log_filename,
-                            format='%(levelname)s:%(message)s',
-                            level=logging.DEBUG)
+def server_db_retrieve(args):
+    print('Retrieving tables from database.')
+    machines = []
+    members = []
+    cards = []
+    tags = []
     
-        # Get machine names from database
-        if not self.load():
-            return False
+    # Test db data retrieval
+    config_filename = 'db_connect_fields.ignored'
+    db_connect.read_db(machines, members, cards, tags, config_filename)
+    csv_filename = 'access_tables.csv'
+    print('Writing access tables to file.')
+    csv_rw.member_access_write(csv_filename,
+                               machines,
+                               members,
+                               cards,
+                               tags)
+    print('Access tables were written to %s.' % csv_filename)
 
-        # Initialise radio
-        if not self.radio_init():
-            return False
+def server_new_log(args):
+    print('Starting a new software events log file.')
+    # Find existing log
+    log_file = glob('piaccessserver_*.log')
+    logging.shutdown()
+    if len(log_file) > 0:
+        archive_folder_name = 'archives'
+        print('Archiving %s file in folder %s.' % (log_file[0], archive_folder_name))
+        if not os.path.exists(archive_folder_name):
+            os.makedirs(archive_folder_name)
+        shutil.move(log_file[0], archive_folder_name + os.path.sep + log_file[0])
+    # Create another log file
+    log_file = get_sw_log_filename()
+    print('Created new %s file.' % log_file[0])
+    logging.basicConfig(filename=log_file[0],
+                        format='%(asctime)s:%(levelname)s:%(message)s',
+                        level=logging.DEBUG)
+    logging.info('Software events log file creation.')
+    
+def commutator_check(args):
+    print('Commutator %s is fine.' % args.commutator_name)
 
-        # Create a link for each machine
-        for machine in self.machines:
-            # Load machine specific section from config file
-            channel    = self.config.get(machine, 'channel')
-            machine_id = self.config.get(machine, 'machine_id')
-            # Initialise link for machine
-            self.link[machine] = link_command.LinkCommand(self.radio,
-                                                          channel,
-                                                          machine_id)
-        
-    def load(self):
-        # Initialise output arguments
-        machines_db = []
-        members_db = []
-        cards_db = []
-        tags_db = []
-        
-        # Read data from database
-        if db_connect.read_db(machines_db, members_db,
-                              cards_db, tags_db, self.config_filename):
+def commutator_update(args):
+    print('Commutator %s access tables were updated.' % args.commutator_name)
 
-            # If successful, write result in csv file
-            member_access_write(self.csv_filename, machines_db,
-                                members_db, cards_db, tags_db)
+def commutator_on(args):
+    print('Commutator %s is now always on.' % args.commutator_name)
 
-        # In any case, read from csv file
-        machines_csv = []
-        cards_csv = []
-        authorisations_csv = []
-        if not member_access_read(self.csv_filename,
-                                  machines_csv,
-                                  cards_csv,
-                                  authorisations_csv):
-            return False
+def commutator_off(args):
+    print('Commutator %s is now always off.' % args.commutator_name)
 
-        # Update cards list
-        self.cards = cards_csv
-        
-        # Build an access table for each machine found in database AND config file
-        self.machines = []
-        self.authorisations = {}
-        num_machines = 0
-        for machine_num in range(len(machines_csv)):
-            # Check if machine exists in config file
-            machine_name = machines_csv[machine_num]
-            if config.has_section(machine_name):
-                self.machines.append(machine_name)
-                self.authorisations[machine_name] = authorisations_csv[machine_num]
-                num_machines += 1
-        
-        if self.number_machines > num_machines:
-            logging.warning('Some of the machines in config file were not \
-                             found in database.',
-                            num_machines, self.number_machines)
-            self.number_machines = num_machines
-        
-        return True
-        
-    def loop(self, config_filename, radio):
-        # Initialise output arguments
-        machines_db = []
-        members_db = []
-        cards_db = []
-        tags_db = []
-        
-        # Refresh server configuration
-        
-        
-        # Read data from database
-        if not piaccessserver_load():
-            return False
+def commutator_auto(args):
+    print('Commutator %s now activates using cards and access tables.' % args.commutator_name)
 
-        # Update access entries for all machines
-        for machine in self.machines:
-            # Send command
-            status = link_command.update_table([self.cards, self.authorisations[machine]])        
+def commutator_get_log(args):
+    print('Commutator %s logs were retrieved.' % args.commutator_name)
 
-        # Retrieve log entries from all machines
-        
-            # Send command
-            link_command.dump_logging
-            
-            # If there are any entries, write them to local log
+def commutator_new_log(args):
+    print('Commutator %s logs will be saved in a new file.' % args.commutator_name)
 
-        
-        # Check memory usage on all machine
-
-            # Send command
-
-            # If memory exceeds threshold, write warning to local log
-            
-        # Wait for next loop iteration
-        time.sleep(config.getfloat('ACCESS_SERVER', 'loop_delay'))
-
-
-    def radio_init(self, config_filename):
-        # TODO: Check what are those for and validate parameters
-        # TODO: Check for initialisation errors from radio
-        self.pipes = [[0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]]
-
-        self.radio = NRF24(GPIO, spidev.SpiDev())
-        self.radio.begin(0, 25)
-        time.sleep(1)
-        # Set retries (delay, count)
-        self.number_machines = self.config.get('MACHINES',
-                                               'number_machines')
-        self.radio.setRetries(15,15)
-        self.radio.setPayloadSize(32)
-        self.radio.setChannel(0x60)
-
-        self.radio.setDataRate(NRF24.BR_2MBPS)
-        self.radio.setPALevel(NRF24.PA_MIN)
-        self.radio.setAutoAck(True)
-        self.radio.enableDynamicPayloads()
-        self.radio.enableAckPayload()
-
-        self.radio.openWritingPipe(pipes[1])
-        self.radio.openReadingPipe(1, pipes[0])
-        self.radio.printDetails()
-
-        return True
+def get_sw_log_filename():
+    log_file = glob('piaccessserver_*.log')
+    if len(log_file) == 0:
+        # Create file with current time
+        log_file.append('piaccessserver_' + strftime("%Y-%m-%d_%Hh%Mm%Ss") + '.log')
+    return log_file   
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Access server software on Raspberry Pi for managing RFID card readers on machines.')    
+    # Set software event logging
+    log_file = get_sw_log_filename()
+    logging.basicConfig(filename=log_file[0],
+                        format='%(asctime)s:%(levelname)s:%(message)s',
+                        level=logging.DEBUG)
     
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
+    # Create the top-level parser
+    parser = argparse.ArgumentParser(version='1.0', description='Access server software for managing RFID commutators.')    
+    subparsers = parser.add_subparsers()
     
-    config_filename = 'piaccessserver.ini'
-    server = PiAccessServer(config_filename)
-    # Initialise server
-    if not server.piaccessserver_init():
-        sys.exit('Initialisation failed. Check log for details.')
+    # Create subcommand parsers
+    parser_sdr = subparsers.add_parser('server_db_retrieve', description='Retrieves access tables from the database.')
+    parser_sdr.set_defaults(func=server_db_retrieve)
 
-    # Process server tasks
-    while True:
-        
-        # Process tasks
-        if not piaccessserver_loop(config_filename):
-            sys.exit('Main loop failed. Check log for details.')
+    parser_sdr = subparsers.add_parser('server_new_log', description='Backups current server software events log and starts a new one with the current date.')
+    parser_sdr.set_defaults(func=server_new_log)
 
+    commutator_name_help = 'Name of the commutator as defined in the configuration file (default is all commutators).'    
+    parser_sdr = subparsers.add_parser('commutator_check', description='Backups current server software events log and starts a new one with the current date.')
+    parser_sdr.add_argument('-n', '--commutator_name', default='all', help=commutator_name_help)
+    parser_sdr.set_defaults(func=commutator_check)
         
-         
+    parser_sdr = subparsers.add_parser('commutator_update', description='Updates the access tables of a commutator.')
+    parser_sdr.add_argument('-n', '--commutator_name', default='all', help=commutator_name_help)
+    parser_sdr.set_defaults(func=commutator_update)
+        
+    parser_sdr = subparsers.add_parser('commutator_on', description='Sets commutator as always on.')
+    parser_sdr.add_argument('-n', '--commutator_name', default='all', help=commutator_name_help)
+    parser_sdr.set_defaults(func=commutator_on)
+        
+    parser_sdr = subparsers.add_parser('commutator_off', description='Sets commutator as always off.')
+    parser_sdr.add_argument('-n', '--commutator_name', default='all', help=commutator_name_help)
+    parser_sdr.set_defaults(func=commutator_off)
+        
+    parser_sdr = subparsers.add_parser('commutator_auto', description='Sets commutator activation to automatic (using card reader and access tables).')
+    parser_sdr.add_argument('-n', '--commutator_name', default='all', help=commutator_name_help)
+    parser_sdr.set_defaults(func=commutator_auto)
+        
+    parser_sdr = subparsers.add_parser('commutator_get_log', description='Gets commutator events log.')
+    parser_sdr.add_argument('-n', '--commutator_name', default='all', help=commutator_name_help)
+    parser_sdr.set_defaults(func=commutator_get_log)
+        
+    parser_sdr = subparsers.add_parser('commutator_new_log', description='Backups current commutator events log and starts a new one with the current date.')
+    parser_sdr.add_argument('-n', '--commutator_name', default='all', help=commutator_name_help)
+    parser_sdr.set_defaults(func=commutator_new_log)
+        
+    args = parser.parse_args()
+    args.func(args)
 
         
         
---
+++ piaccessserver/piaccessserver_old.py
@@ -0,0 +1,211 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# The server main program
+#
+
+import sys
+import ConfigParser
+import time
+import logging
+import argparse
+
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+from lib_nrf24 import NRF24
+import time
+import spidev
+
+import db_connect
+import link_command
+import csv_csvw
+
+class PiAccessServer():
+    """ Main application class for the access server of the RFID card
+    control system.
+    """
+    def __init__(self, config_filename):
+        # Get server parameters
+        self.config_filename = config_filename
+        self.config = ConfigParser.RawConfigParser()    
+        self.config.read(self.config_filename)
+        
+        self.name = self.config.get('ACCESS_SERVER',
+                                    'name')
+        self.csv_filename = self.config.get('ACCESS_SERVER',
+                                            'csv_filename')
+        self.log_filename = self.config.get('ACCESS_SERVER',
+                                               'log_filename')
+        self.number_machines = self.config.get('MACHINES',
+                                               'number_machines')
+        self.mem_usage_threshold = self.config.getfloat('MACHINES',
+                                               'mem_usage_threshold')
+
+        # Initialise log file (time not added by default because machine
+        # log entries have a differed timestamp
+        logging.basicConfig(filename=self.log_filename,
+                            format='%(levelname)s:%(message)s',
+                            level=logging.DEBUG)
+    
+        # Get machine names from database
+        if not self.load():
+            return False
+
+        # Initialise radio
+        if not self.radio_init():
+            return False
+
+        # Create a link for each machine
+        for machine in self.machines:
+            # Load machine specific section from config file
+            channel    = self.config.get(machine, 'channel')
+            machine_id = self.config.get(machine, 'machine_id')
+            # Initialise link for machine
+            self.link[machine] = link_command.LinkCommand(self.radio,
+                                                          channel,
+                                                          machine_id)
+        
+    def load(self):
+        # Initialise output arguments
+        machines_db = []
+        members_db = []
+        cards_db = []
+        tags_db = []
+        
+        # Read data from database
+        if db_connect.read_db(machines_db, members_db,
+                              cards_db, tags_db, self.config_filename):
+
+            # If successful, write result in csv file
+            member_access_write(self.csv_filename, machines_db,
+                                members_db, cards_db, tags_db)
+
+        # In any case, read from csv file
+        machines_csv = []
+        cards_csv = []
+        authorisations_csv = []
+        if not member_access_read(self.csv_filename,
+                                  machines_csv,
+                                  cards_csv,
+                                  authorisations_csv):
+            return False
+
+        # Update cards list
+        self.cards = cards_csv
+        
+        # Build an access table for each machine found in database AND config file
+        self.machines = []
+        self.authorisations = {}
+        num_machines = 0
+        for machine_num in range(len(machines_csv)):
+            # Check if machine exists in config file
+            machine_name = machines_csv[machine_num]
+            if config.has_section(machine_name):
+                self.machines.append(machine_name)
+                self.authorisations[machine_name] = authorisations_csv[machine_num]
+                num_machines += 1
+        
+        if self.number_machines > num_machines:
+            logging.warning('Some of the machines in config file were not \
+                             found in database.',
+                            num_machines, self.number_machines)
+            self.number_machines = num_machines
+        
+        return True
+        
+    def loop(self, config_filename, radio):
+        # Initialise output arguments
+        machines_db = []
+        members_db = []
+        cards_db = []
+        tags_db = []
+        
+        # Refresh server configuration
+        
+        
+        # Read data from database
+        if not piaccessserver_load():
+            return False
+
+        # Update access entries for all machines
+        for machine in self.machines:
+            # Send command
+            status = link_command.update_table([self.cards, self.authorisations[machine]])        
+
+        # Retrieve log entries from all machines
+        
+            # Send command
+            link_command.dump_logging
+            
+            # If there are any entries, write them to local log
+
+        
+        # Check memory usage on all machine
+
+            # Send command
+
+            # If memory exceeds threshold, write warning to local log
+            
+        # Wait for next loop iteration
+        time.sleep(config.getfloat('ACCESS_SERVER', 'loop_delay'))
+
+
+    def radio_init(self, config_filename):
+        # TODO: Check what are those for and validate parameters
+        # TODO: Check for initialisation errors from radio
+        self.pipes = [[0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]]
+
+        self.radio = NRF24(GPIO, spidev.SpiDev())
+        self.radio.begin(0, 25)
+        time.sleep(1)
+        # Set retries (delay, count)
+        self.number_machines = self.config.get('MACHINES',
+                                               'number_machines')
+        self.radio.setRetries(15,15)
+        self.radio.setPayloadSize(32)
+        self.radio.setChannel(0x60)
+
+        self.radio.setDataRate(NRF24.BR_2MBPS)
+        self.radio.setPALevel(NRF24.PA_MIN)
+        self.radio.setAutoAck(True)
+        self.radio.enableDynamicPayloads()
+        self.radio.enableAckPayload()
+
+        self.radio.openWritingPipe(pipes[1])
+        self.radio.openReadingPipe(1, pipes[0])
+        self.radio.printDetails()
+
+        return True
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(version='1.0', description='Access server software for managing RFID commutators.')    
+    parser.add_argument('--server_db_retrieve', type=int)
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+    
+    config_filename = 'piaccessserver.ini'
+    server = PiAccessServer(config_filename)
+    # Initialise server
+    if not server.piaccessserver_init():
+        sys.exit('Initialisation failed. Check log for details.')
+
+    # Process server tasks
+    while True:
+        
+        # Process tasks
+        if not piaccessserver_loop(config_filename):
+            sys.exit('Main loop failed. Check log for details.')
+
+        
+         
+
+        
+        
